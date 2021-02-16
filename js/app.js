47b5c5053af4c42a381e9bf9ffe8f18b06f7c03e
Xurmusha <juraboeva98@inbox.ru>
2021-02-16 16:10:01 +0500

    Konstruktor

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function drawBorder() {
+    document.getElementById('borderdraw').style.border='2px solid #E25725';
+    document.getElementById('borderdraw').style.backgroundColor='white';
+    document.getElementById('borderdraw').style.borderRadius='12px';
+}
+
+function drawBorderForUnique() {
+    document.getElementById('uniqueBorder').style.border='2px solid #E25725';
+    document.getElementById('uniqueBorder').style.backgroundColor='white';
+    document.getElementById('uniqueBorder').style.borderRadius='12px';
+}
